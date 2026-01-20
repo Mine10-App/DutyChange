ab35cebeefc5d91aa9fd2b574633085c5f8bc64d
Mine <air1345@gmail.com>
2026-01-20 13:08:17 +0500

    Update user.js

--- user.js
+++ user.js
@@ -1,5 +1,5 @@
-// user.js (alternative version)
-export const users = [
+// user.js
+const users = [
   { username: "Irufan", name: "Abdulla Irufan", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "Supervisor" },
   { username: "Hussain", name: "Hussain Ahmed", passwordHash: "ae64da427100874dd9d1d5b7fc587c41144d42ff23e45d9d075189b9e5fa53ca", Level: "Supervisor" },
   { username: "Mohamed", name: "Mohamed Ibrahim", passwordHash: "c75f28325cfa028ea13872f977a29e0e87c99a4f390fe260f24d7e1f05fb8d75", Level: "Supervisor" },
