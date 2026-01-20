a8bf23a1c1a273d9ff9d96ff14d15547a24b0d35
Mine <air1345@gmail.com>
2026-01-20 14:04:07 +0500

    Create leavebalance.js

---
+++ leavebalance.js
@@ -0,0 +1,8 @@
+// leavebalance.js
+const leavebalance = [
+  { username: "Irufan", name: "Abdulla Irufan", FRL: 10, SL: 30, Annual: 30, RCNo: "979" },
+  { username: "Hussain", name: "Hussain Ahmed", FRL: 10, SL: 30, Annual: 30, RCNo: "001" },
+  { username: "Mohamed", name: "Mohamed Ibrahim", FRL: 10, SL: 30, Annual: 30, RCNo: "002" },
+  { username: "fiyaza", name: "Fiyaza Abdulla", FRL: 10, SL: 30, Annual: 30, RCNo: "003" },
+  { username: "A-6313", name: "Aminath Almas", FRL: 10, SL: 30, Annual: 30, RCNo: "004" },
+];
