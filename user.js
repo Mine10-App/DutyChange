685edea3c8b903c3afd5736b3e449e5256797d95
Mine <air1345@gmail.com>
2026-01-22 13:31:12 +0500

    Update user.js

--- user.js
+++ user.js
@@ -27,8 +27,8 @@ const users = [
   { username: "Ruhushath", name: "Ruhushath Ahmed", passwordHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",  Level: "User", RCNo: "A-8089" },
   { username: "Shiara", name: "Shiara Saudulla", passwordHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",  Level: "User", RCNo: "A-7503" },
    { username: "Soba", name: "Mohamed Sobah", passwordHash: "601e7aa858d36beadd3cc282de3afd3254397d48ccae8c2bedadcf04a51f0c2f", Level: "User", RCNo: "T-0160" },
-  { username: "Irufan1", name: "Abdulla Irufaan", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "User", RCNo: "978" },
-  { username: "Irufan2", name: "Abdulla Irfan", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "User", RCNo: "977" },
+  { username: "Mushrifa", name: "Musrifa Riyaz", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "User", RCNo: "T-0112" },
+  { username: "Sadhina", name: "Sadhina Mohamed", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "User", RCNo: "A-10886" },
 
   
  
