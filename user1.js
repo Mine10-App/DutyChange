09ca770ab655fdad72c5dd38d703cf58e1138dc8
Mine <air1345@gmail.com>
2026-01-24 15:36:45 +0500

    Create user1.js

---
+++ user1.js
@@ -0,0 +1,9 @@
+const users = [
+  { 
+    username: "Irufan", 
+    name: "Abdulla Irufan", 
+    passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", 
+    Level: "Supervisor",  
+    RCNo: "979"
+  }
+];
